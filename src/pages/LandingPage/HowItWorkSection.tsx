17655d723954468c7b29a27d27b121a47d6dc250
kratiDatacode <krati.datacode@gmail.com>
2025-07-10 14:23:14 +0530

    Task krati : add styles for left-side (how-it-works)

--- src/pages/LandingPage/HowItWorkSection.tsx
+++ src/pages/LandingPage/HowItWorkSection.tsx
@@ -4,9 +4,13 @@ const HowItWorkSection = () => {
       <section className="row mx-0 my-5 py-5 d-flex justify-content-center">
         <div className="col-md-10 col-12">
           <div className="row mx-0">
-            <div className="col-5 px-3 px-md-2">
-              <h1>How PartyVerse Works</h1>
-              <p> Book your perfect party in just a few simple steps</p>
+            <div className="col-md-5 col-12 px-3 px-md-2">
+              <h1 className="custom-title">
+                How <span>PartyVerse</span> Works
+              </h1>
+              <p className="mb-5 custom-content">
+                Book your perfect party in just a few simple steps
+              </p>
               <div className="d-flex flex-column gap-3">
                 {[
                   "Discover the Perfect Party Place",
@@ -16,16 +20,21 @@ const HowItWorkSection = () => {
                 ].map((text, index) => (
                   <div
                     key={index}
-                    className="d-flex align-items-start gap-3 step-box"
+                    className="d-flex align-items-start mb-4 p-3 p-md-3 bg-light rounded shadow-sm custom-content step-box"
                   >
-                    <div className="step-number">{index + 1}</div>
-                    <p className="mb-0">{text}</p>
+                    <div
+                      className="d-flex justify-content-center align-items-center rounded-circle ratio ratio-1x1 step-number"
+                    >
+                      {index + 1}
+                    </div>
+                    <div className="d-flex flex-column justify-content-center align-items-start px-3">
+                      <p className="mb-0">{text}</p>
+                    </div>
                   </div>
                 ))}
               </div>
             </div>
-            <div className="col-7 px-3 px-md-2"></div>
-            
+            <div className="col-md-7 col-12 px-3 px-md-2"></div>
           </div>
         </div>
       </section>
