7f19654e8403507b50986518c5670fc39fc52a71
ishavaishnav9926 <divyavaishnav0720@gmail.com>
2025-07-11 18:04:41 +0530

    add footer

---
+++ src/module/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import "./style.scss"
+const Footer = () =>{
+    return(
+        <>
+         <div className="row">
+            <div className="col-4 border">
+               hello 
+            </div>
+            <div className="col-8 border">
+               hii
+            </div>
+         </div>
+        </>
+    )
+}
+
+export default Footer;
--- src/module/Header/Header.tsx
+++ src/module/Header/Header.tsx
@@ -7,15 +7,15 @@ import Image from 'next/image';
 const Header = () => {
   return (
     <header className="header ">
-      <div className="header-inner">
+      <div className="header-inner m-0 p-0 ">
       <div className="">
         <Image src={PARTYVERSE} alt="logo" />
       </div>
-      <div className="d-flex align-items-center">
-        <div className="header-select">
+      <div className="d-flex align-items-center ">
+        <div className="header-select me-3 ">
           <SelectDropdown label="City" place_holder="Indore" />
         </div>
-        <div className="m-3">
+        <div className="my-3">
           <Button label="Login/Sign up"/>
         </div>
       </div>
