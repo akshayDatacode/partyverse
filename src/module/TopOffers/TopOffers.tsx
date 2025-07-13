528a847bb4cd4a0da7ccb7551077918b7a78223b
akshayDatacode <akshay.webllisto@gmail.com>
2025-07-13 12:41:05 +0530

    handle top offers card partial and view more

--- src/module/TopOffers/TopOffers.tsx
+++ src/module/TopOffers/TopOffers.tsx
@@ -6,8 +6,8 @@ import "./style.scss";
 const TopOffersResponsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-    partialVisibilityGutter: 40 // this is needed to tell the amount of px that should be visible.
+    items: 2,
+    partialVisibilityGutter: 180 // this is needed to tell the amount of px that should be visible.
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
--- src/pages/LandingPage/components/TopOffersSection.tsx
+++ src/pages/LandingPage/components/TopOffersSection.tsx
@@ -1,10 +1,14 @@
 import TopOffers from "@/module/TopOffers";
+import Link from "next/link";
 
 const TopOffersSection = () => {
   return (
     <div className="row mx-0 d-flex justify-content-end my-5 py-5">
       <div className="col-md-11 col-12 px-0">
-        <h2 className="top-offer-heading p-3">Top Offers (7)</h2>
+        <div className="d-flex justify-content-between align-items-center">
+          <h2 className="top-offer-heading p-3">Top Offers (7)</h2>
+          <Link href={'/venues'}><p className="me-5">view more</p></Link>
+        </div>
         <TopOffers />
       </div>
     </div>
