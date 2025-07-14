c6026d50e497305b1171f660d6a4b108feb3295c
akshayDatacode <akshay.webllisto@gmail.com>
2025-07-14 22:46:31 +0530

    Task: handle and improve Moment section

--- src/pages/LandingPage/components/MomentsSection.tsx
+++ src/pages/LandingPage/components/MomentsSection.tsx
@@ -3,7 +3,7 @@ import MomentsBackground from "@/assets/images/MomentsBackground.png";
 import BirthdayMoment from "@/assets/images/birthday-moment.png";
 import KittyMoment from "@/assets/images/kitty-moment.png";
 import BirthdayMoment2 from "@/assets/images/bithday-moment2.png";
-import BirthdayMoment3 from "@/assets/images/bithday-moment3.png";
+import BirthdayMoment3 from "@/assets/images/birthday-moment3.jpg";
 import MultiCarousel from "@/ui/Carousel/MultiCarousel";
 import Image from "next/image";
 
@@ -28,27 +28,30 @@ const MomentsSection = () => {
       people: "100 People",
     },
     {
-      src: BirthdayMoment3,
+      src: KittyMoment,
       title: "Birthday Party Hosted at",
       restaurant: "Restaurant Name",
       people: "100 People",
     },
   ];
-  const imageResponsive = {
+  const MomentResponsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 4,
-      slidesToSlide: 4,
+      items: 3,
+      slidesToSlide: 1,
+      partialVisibilityGutter: 80
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
       slidesToSlide: 2,
+      partialVisibilityGutter: 180
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
       slidesToSlide: 1,
+      partialVisibilityGutter: 180
     },
   };
   return (
@@ -64,30 +67,34 @@ const MomentsSection = () => {
           priority
           style={{ objectFit: "cover" }}
         />
-    
-        <div className="position-relative p-2">
-        <div className="row d-flex flex-column justify-content-center align-items-center ">
-         <div className="col-md-4">
-          <Image src={PartyVerse} alt="logo" className="mt-3" />
-          <h1 className="fw-bold heading">Moments.</h1>
-          <p className="text-white text-center">
-            Celebrating every moment of your life.
-          </p>
+
+        <div className="row mx-0 d-flex justify-content-center align-items-center">
+          <div className="col-12">
+            <div className="text-center">
+              <Image src={PartyVerse} alt="logo" className="mt-3" />
+              <h1 className="fw-bold text-center heading">Moments.</h1>
+            </div>
+            <p className="text-white text-center">
+              Celebrating every moment of your life.
+            </p>
           </div>
-           <div className="col-md-10 mt-5 d-flex justify-content-end">
+        </div>
+        <div className="row mx-0 d-flex justify-content-end">
+          <div className="col-md-11 px-0 mt-5">
             <MultiCarousel
-              responsive={imageResponsive}
+              responsive={MomentResponsive}
               arrows={false}
               autoPlay={false}
               draggable={true}
+              showDots={false}
+              partialVisible={true}
             >
               {carouselImages.map((item, index) => (
-                <div className="moment-card" key={index}>
+                <div className="px-3" key={index}>
                   <Image
                     src={item.src}
                     alt={`Moment ${index + 1}`}
                     className="img-fluid rounded-4 mb-3"
-                     style={{ height: "572px", width:"322px" }}
                   />
                   <p className="card-text mb-0">{item.title}</p>
                   <h5 className="restaurant-name fw-bold">{item.restaurant}</h5>
@@ -95,7 +102,6 @@ const MomentsSection = () => {
                 </div>
               ))}
             </MultiCarousel>
-           </div>
           </div>
         </div>
       </div>
