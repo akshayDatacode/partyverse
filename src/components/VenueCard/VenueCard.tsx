cc505e63fd10464cba0ca6679092204c691fbd53
kratiDatacode <krati.datacode@gmail.com>
2025-07-14 15:58:33 +0530

    Task krati :fix badgetype for venue card component

--- src/components/VenueCard/VenueCard.tsx
+++ src/components/VenueCard/VenueCard.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
-import './style.scss';
+import "./style.scss";
+import Venuecard from "@/assets/images/venuecards.png";
+import Logistic from "@/assets/images/logistics.png"
+import MultiCarousel from "@/ui/Carousel";
 
 type VenueCardProps = {
   image: string;
-  badgeType?: "luxe" | "new" | "promoted" | ""; 
+  badgeType?: "luxe" | "new" | "promoted" | "";
   discount?: string;
   rating: number;
   distance: string;
@@ -16,71 +19,103 @@ type VenueCardProps = {
 
 const VenueCard = ({
   image,
-  badgeType = "",
-  discount,
-  rating=0,
-  distance="0 KM",
+  badgeType = "luxe",
+  discount = "50%",
+  rating = 0,
+  distance = "0 KM",
   venueName,
-  location,
-  capacity=0,
-  packages=0,
-  cuisine,
+  location="jaipur",
+  capacity = 0,
+  packages = 0,
+  cuisine="indian",
 }: VenueCardProps) => {
+  
+   const CardImageResponsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+      slidesToSlide: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+      slidesToSlide: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1
+    }
+  };
   return (
     <>
       <div className="bg-white position-relative overflow-hidden venue-card">
-      {/* Badge */}
-      {badgeType && (
-        <span className={`badge  position-absolute top-0 start-0 m-2 px-3 py-2 tag-badge ${badgeType}`}>
-          {badgeType.charAt(0).toUpperCase() + badgeType.slice(1)}
-        </span>
-      )}
-
-      {/* Image */}
-      <div className="position-relative">
-        <Image
-          src={image}
-          alt={venueName}
-          width={342}
-          height={286}
-          className="img-fluid w-100"
-          style={{ objectFit: "cover", height: "200px" }}
-        />
-        {discount && (
-          <span className="fw-bold position-absolute bottom-0 start-0 p-2 m-2 discount-badge">
-            {discount}
+        {/* Badge */}
+        {badgeType && (
+          <span
+            className={`badge  position-absolute top-0 start-0 m-2 px-3 py-2 tag-badge ${badgeType}`}
+          >
+            {badgeType.charAt(0).toUpperCase() + badgeType.slice(1)}
           </span>
         )}
-      </div>
 
-      {/* Content */}
-      <div className="p-3">
-        {/* Rating + Distance */}
-        <div className="d-flex align-items-center gap-2 mb-2">
-          <span className="badge fw-medium rating">{rating.toFixed(1)} ★</span>
-          <small className="fw-medium distance">{distance}</small>
+        {/* Image */}
+        <div className="position-relative">
+          <MultiCarousel responsive={CardImageResponsive} arrows={false} autoPlay={false} draggable={true} >
+            <Image
+              src={image || Venuecard}
+              alt={venueName}
+              width={342}
+              height={286}
+              className="img-fluid w-100"
+              style={{ objectFit: "cover", height: "200px" }}
+            />
+              <Image
+              src={image || Logistic}
+              alt={venueName}
+              width={342}
+              height={286}
+              className="img-fluid w-100"
+              style={{ objectFit: "cover", height: "200px" }}
+            />
+          </MultiCarousel>
+          {discount && (
+            <span className="fw-bold position-absolute bottom-0 start-0 p-2 m-2 discount-badge">
+              {discount}
+            </span>
+          )}
         </div>
 
-        {/* Name + Location */}
-        <h5 className="mb-1 fw-bold venue-name">{venueName}</h5>
-        <p className="fw-medium location">{location}</p>
+        {/* Content */}
+        <div className="p-3">
+          {/* Rating + Distance */}
+          <div className="d-flex align-items-center gap-2 mb-2">
+            <span className="badge fw-medium rating">
+              {rating.toFixed(1)} ★
+            </span>
+            <small className="fw-medium distance">{distance}</small>
+          </div>
 
-        {/* Capacity + Packages */}
-        <div className="d-flex gap-2 flex-wrap mb-2">
-          <span className="badge capacity">
-            Max Capacity <strong>{capacity}</strong>
-          </span>
-          <span className="badge capacity">
-            <strong>{packages}</strong> Packages
-          </span>
+          {/* Name + Location */}
+          <h5 className="mb-1 fw-bold venue-name">{venueName}</h5>
+          <p className="fw-medium location">{location}</p>
+
+          {/* Capacity + Packages */}
+          <div className="d-flex gap-2 flex-wrap mb-2">
+            <span className="badge capacity">
+              Max Capacity <strong>{capacity}</strong>
+            </span>
+            <span className="badge capacity">
+              <strong>{packages}</strong> Packages
+            </span>
+          </div>
+
+          {/* Cuisine */}
+          <p className="mb-0 cuisine">
+            Cuisine Served: <span className="text-muted">{cuisine}</span>
+          </p>
         </div>
-
-        {/* Cuisine */}
-        <p className="mb-0 cuisine">
-          Cuisine Served: <span className="text-muted">{cuisine}</span>
-        </p>
       </div>
-    </div>
     </>
   );
 };
