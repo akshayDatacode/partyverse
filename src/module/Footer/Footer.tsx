cd8fcb33e836a22e536cb42b8f2d3a5e241da754
kratiDatacode <krati.datacode@gmail.com>
2025-07-21 13:19:50 +0530

    Task krati : add navigation links in footer

--- src/module/Footer/Footer.tsx
+++ src/module/Footer/Footer.tsx
@@ -1,11 +1,11 @@
-'use client'
-import Image from "next/image"
-
-import Twitter from "@/assets/images/X Twitter Logo Streamline Logos Solid - Free.png"
-import Instagram from "@/assets/images/Instagram Streamline Feather.png"
-import Facebook from "@/assets/images/Facebook 1 Streamline Plump Line - Free.png"
-import PARTYVERSE from "@/assets/images/partyverse.png"
-import "./style.scss"
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import Twitter from "@/assets/images/X Twitter Logo Streamline Logos Solid - Free.png";
+import Instagram from "@/assets/images/Instagram Streamline Feather.png";
+import Facebook from "@/assets/images/Facebook 1 Streamline Plump Line - Free.png";
+import PARTYVERSE from "@/assets/images/partyverse.png";
+import "./style.scss";
 
 const Footer = () => {
   return (
@@ -18,11 +18,23 @@ const Footer = () => {
                 <div className="col-md-4 col-12 ps-md-0 pe-md-4 px-4 py-md-4 py-3 footer-section-1">
                   <div className="">
                     <Image src={PARTYVERSE} className="my-3" alt="Logo" />
-                    <p className="my-2 footer-paragraph">Your one-stop solution for stress-free party planning. We handle the details, you create the memories.</p>
+                    <p className="my-2 footer-paragraph">
+                      Your one-stop solution for stress-free party planning. We
+                      handle the details, you create the memories.
+                    </p>
                     <div className="d-flex flex-column gap-3 py-4 social-media">
-                      <span><Image src={Instagram} alt="" className=" me-1" />Instagram</span>
-                      <span><Image src={Facebook} alt="" className=" me-1" />Facebook</span>
-                      <span><Image src={Twitter} alt="" className=" me-1" />Twitter</span>
+                      <span>
+                        <Image src={Instagram} alt="" className=" me-1" />
+                        Instagram
+                      </span>
+                      <span>
+                        <Image src={Facebook} alt="" className=" me-1" />
+                        Facebook
+                      </span>
+                      <span>
+                        <Image src={Twitter} alt="" className=" me-1" />
+                        Twitter
+                      </span>
                     </div>
                   </div>
                 </div>
@@ -30,7 +42,9 @@ const Footer = () => {
                   <div className="row mx-0 justify-content-between p-md-4">
                     <div className="col-4 services">
                       <div className="main-data">Services</div>
+                      <Link href={"/venues"} className="text-decoration-none">
                       <span className="sub-data">Venues</span>
+                      </Link>
                       <span className="sub-data">Food Packages</span>
                       <span className="sub-data">Decoration</span>
                       <span className="sub-data">Photography</span>
@@ -52,13 +66,15 @@ const Footer = () => {
                       <span className="sub-data">Careers</span>
                     </div>
                   </div>
-
                 </div>
               </div>
             </div>
             <div className="col-12 px-md-0 px-4 my-md-0 my-4">
               <div className="row mx-0 d-flex">
-                <div className="col-md-4 px-0 footer-paragraph footer-end">© 2025 <span className="partyverse">Partyverse</span> All Rights Reserved.</div>
+                <div className="col-md-4 px-0 footer-paragraph footer-end">
+                  © 2025 <span className="partyverse">Partyverse</span> All
+                  Rights Reserved.
+                </div>
                 <div className="col-md-8 px-md-5 px-0 py-2 py-md-0 footer-paragraph">
                   <span className="me-5">Privacy Policy</span>
                   <span>Terms of Use</span>
@@ -69,7 +85,7 @@ const Footer = () => {
         </div>
       </footer>
     </>
-  )
-}
+  );
+};
 
 export default Footer;
