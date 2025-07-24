a3d433832dc502932b74820e88d5f3fc60c8bb98
kratiDatacode <krati.datacode@gmail.com>
2025-07-24 12:30:54 +0530

    Task krati : improve landing page drawer

--- src/module/SearchPartyFilter/SelectPartyFilter.tsx
+++ src/module/SearchPartyFilter/SelectPartyFilter.tsx
@@ -8,25 +8,29 @@ type SelectProps = {
 
 const SelectPartyFilter = ({ actionEvent = false }: SelectProps) => {
   return (
-    <div className="row mx-0 ">
+    <div className="row mx-0">
       {[{ lab: "Location Type", placeholder: "Fine Dine" },
         {lab: "Celebration Date", placeholder: "23/03/2025"},
         {lab: "Occasion", placeholder: "Get Together"},
          ].map((item, index) => {
         return (
-          <div className={`${actionEvent ? "col-2" : " col-3"} text-center select-party p-3 ${index===0 ? "select-start":""}`} key={index}>
+          <div className={`col-6 ${actionEvent ? "col-md-2" : "col-md-3"} text-center select-party p-3 ${index===0 ? "select-start":""}`} key={index}>
             <SelectDropdown label={item.lab} place_holder={item.placeholder} />
           </div>
         )
       })}
-      <div className={`text-center py-3 position-relative select-party ${actionEvent ? "col-2" : "select-end col-3"}`}>
+      <div className={`text-center py-3 position-relative select-party col-6 ${actionEvent ? "col-md-2" : "select-end col-md-3"}`}>
         <SelectDropdown label="Number of Guests" place_holder="100" />
         <div className="position-absolute bg-dark text-white fw-medium px-2 py-1 fs-10 tentative-number lh-100 ls-0 font-style-medium">just a tentative number!</div>
       </div>
-      {actionEvent ?
-        <div className="col-2 px-0 py-3 text-center d-flex align-items-center justify-content-center select-end select-party">
+      {actionEvent ?(
+        <div className="col-6 col-md-2 px-0 py-3 text-center d-flex align-items-center justify-content-center select-end select-party">
           <Button label="Edit Search" />
-        </div> : ""
+        </div> ):(
+            <div className=" col-12  d-md-none d-block mt-5">
+          <Button label="Search Now" />
+        </div>
+        )
       }
     </div>
   );
