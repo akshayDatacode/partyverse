df2e3160bffdca6afe1f38c77e1efdba62634bd2
kratiDatacode <krati.datacode@gmail.com>
2025-07-24 16:47:57 +0530

    Task krati : add badges at top in  drawer

--- src/pages/VenueListingPage/components/VenuListFilter.tsx
+++ src/pages/VenueListingPage/components/VenuListFilter.tsx
@@ -1,8 +1,9 @@
-'use client';
+"use client";
 import Image from "next/image";
 
 import filterIcon from "@/assets/images/Filter--Streamline-Ionic-Filled.svg.png";
 import VenueFilterItem from "@/components/VenueFilter/VenueFilterItem";
+import BadgeTag from "@/components/BadgeTag";
 import { useState } from "react";
 
 type Props = {
@@ -11,7 +12,9 @@ type Props = {
 };
 
 const VenuListFilter = ({ setShowFilters, showFilters }: Props) => {
-  const [selectedFilters, setSelectedFilters] = useState<Record<string, string>>({});
+  const [selectedFilters, setSelectedFilters] = useState<
+    Record<string, string>
+  >({});
 
   const handleFilterChange = (category: string, value: string) => {
     setSelectedFilters((prevFilters) => ({
@@ -28,62 +31,128 @@ const VenuListFilter = ({ setShowFilters, showFilters }: Props) => {
           onClick={() => setShowFilters(false)}
         />
       )}
-      <pre>{JSON.stringify(selectedFilters, null, 2)}</pre>
+
+      {/* Badges */}
+      {/* <pre>{JSON.stringify(selectedFilters, null, 2)}</pre> */}
+      <div className="filter-badges-wrapper d-flex gap-2 overflow-auto flex-nowrap hide-scrollbar mb-3 px-2">
+        {Object.entries(selectedFilters).map(([key, value], i) => (
+          <BadgeTag
+            key={i}
+            label={value}
+            onClick={() => {
+              const updatedFilters = { ...selectedFilters };
+              delete updatedFilters[key]; // Remove clicked filter
+              setSelectedFilters(updatedFilters);
+            }}
+          />
+        ))}
+      </div>
+
+
+      {/* filters  */}
       <section className="p-2 position-relative bg-white z-md-n1 z-3 ">
         <div className="pb-5">
           <div className="venue-filter-heading pb-2 d-flex justify-content-between">
             <div>
-              <Image src={filterIcon} alt="Filter Icon" height={20} width={20} className="me-2 mb-1" />
+              <Image
+                src={filterIcon}
+                alt="Filter Icon"
+                height={20}
+                width={20}
+                className="me-2 mb-1"
+              />
               Top Filters
             </div>
           </div>
           {["Fine Dine", "Casual Dining", "Pure Veg"].map((label, i) => (
             <VenueFilterItem
-              key={i} label={label} name={'topFilter'}
+              key={i}
+              label={label}
+              name={"topFilter"}
               onChange={() => handleFilterChange("topFilter", label)}
             />
           ))}
         </div>
         <div>
           <div className="pb-3 venue-filter-heading">
-            <Image src={filterIcon} alt="Filter Icon" height={20} width={20} className="me-2 mb-1" />
+            <Image
+              src={filterIcon}
+              alt="Filter Icon"
+              height={20}
+              width={20}
+              className="me-2 mb-1"
+            />
             Other Filters
           </div>
           <div className="pb-2">
-            <div className="py-2 venue-filter-sub-heading">Budget per Guest</div>
+            <div className="py-2 venue-filter-sub-heading">
+              Budget per Guest
+            </div>
             <div className="row">
-            {["₹500 - ₹999", "₹1000 - ₹1299", "₹1300 - ₹1599", "₹1600 - ₹1999", "₹2000 - ₹2499"].map((label, i) => (
-               <div className="col-6 col-md-12 mb-2 mb-md-1" key={i}>
-              <VenueFilterItem key={i} label={label} name={"budget"} 
-              onChange={() => handleFilterChange("budget", label)}
-              />
-                 </div> 
-            ))}
-          </div>
+              {[
+                "₹500 - ₹999",
+                "₹1000 - ₹1299",
+                "₹1300 - ₹1599",
+                "₹1600 - ₹1999",
+                "₹2000 - ₹2499",
+              ].map((label, i) => (
+                <div className="col-6 col-md-12 mb-2 mb-md-1" key={i}>
+                  <VenueFilterItem
+                    key={i}
+                    label={label}
+                    name={"budget"}
+                    onChange={() => handleFilterChange("budget", label)}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
           <div className="pb-2">
-            <div className="py-2 venue-filter-sub-heading ">Restaurant Type</div>
-               <div className="row">
-              {["Fine Dine", "Casual Dining", "Buffet", "Others", "Pub", "Restro Club", "Night Club"].map((label, i) => (
-              <div className="col-6 col-md-12 mb-2 mb-md-1" key={i}>
-              <VenueFilterItem key={i} label={label} name={'restaurant'} 
-              onChange={() => handleFilterChange("restaurant", label)}
-              />
-              </div> 
-            ))}
-          </div>
+            <div className="py-2 venue-filter-sub-heading ">
+              Restaurant Type
+            </div>
+            <div className="row">
+              {[
+                "Fine Dine",
+                "Casual Dining",
+                "Buffet",
+                "Others",
+                "Pub",
+                "Restro Club",
+                "Night Club",
+              ].map((label, i) => (
+                <div className="col-6 col-md-12 mb-2 mb-md-1" key={i}>
+                  <VenueFilterItem
+                    key={i}
+                    label={label}
+                    name={"restaurant"}
+                    onChange={() => handleFilterChange("restaurant", label)}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
           <div className="pb-2">
             <div className="py-2 venue-filter-sub-heading">Cuisine</div>
-             <div className="row">
-            {["Chinese", "North Indian", "South Indian", "Thai", "Restro Club", "Night Club"].map((label, i) => (
-              <div className="col-6 col-md-12 mb-2 mb-md-1" key={i}>
-              <VenueFilterItem key={i} label={label} name={'cuisine'} 
-              onChange={() => handleFilterChange("cuisine", label)}
-              />
-              </div>
-            ))}
-          </div>
+            <div className="row">
+              {[
+                "Chinese",
+                "North Indian",
+                "South Indian",
+                "Thai",
+                "Restro Club",
+                "Night Club",
+              ].map((label, i) => (
+                <div className="col-6 col-md-12 mb-2 mb-md-1" key={i}>
+                  <VenueFilterItem
+                    key={i}
+                    label={label}
+                    name={"cuisine"}
+                    onChange={() => handleFilterChange("cuisine", label)}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </section>
