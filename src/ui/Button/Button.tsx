41032458fe141a000dc0fedf2469ed4bb579d989
kratiDatacode <krati.datacode@gmail.com>
2025-07-25 17:56:02 +0530

    Task krati : improve search place drawer

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -1,24 +1,40 @@
-import './button.scss'
+import "./button.scss";
 
 type ButtonProps = {
   label: string;
   backgroundColor?: string;
   borderColor?: string;
-  type?: 'button' | 'submit' | 'reset';
-}
+  type?: "button" | "submit" | "reset";
+  fullWidth?: boolean;
+  borderRadius?: string;
+  padding?: string;
+};
 
-const Button = ({ label , backgroundColor="", borderColor="", type='button'}: ButtonProps) => {
+const Button = ({
+  label,
+  backgroundColor = "",
+  borderColor = "",
+  type = "button",
+  fullWidth = false,
+  borderRadius = "rounded-2",
+  padding = "p-2",
+}: ButtonProps) => {
   return (
     <>
-      <button type={type} className={`p-2 fw-bold fs-16 lh-100 ls-0 custom-button-primary
-        ${backgroundColor === "white"? "bg-white-btn":""} 
-        ${backgroundColor === "transparent"? "bg-transparent-btn":""}
-        ${borderColor === "primary" ? "border-primary":""}`}
-        >
+      <button
+        type={type}
+        className={`p-2 fw-bold fs-16 lh-100 ls-0 custom-button-primary
+        ${backgroundColor === "white" ? "bg-white-btn" : ""} 
+        ${backgroundColor === "transparent" ? "bg-transparent-btn" : ""}
+        ${borderColor === "primary" ? "border-primary" : ""}
+         ${fullWidth ? "w-100" : ""}
+          ${borderRadius}
+          ${padding}`}
+      >
         {label}
       </button>
     </>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
